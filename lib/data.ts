e1356866ada2643ddf0303d33c15c80f72f20d33
samarthbharadwaj <156901767+samarthbharadwaj@users.noreply.github.com>
2026-09-15 21:37:16 +0530

    Fix: Remove JSX from lib/data.ts to resolve ESLint parsing error

--- lib/data.ts
+++ lib/data.ts
@@ -1,28 +1,3 @@
-import React from "react";
 import { holdings } from "./types";
 
-export default function PortfolioTable() {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Stock</th>
-          {/* eslint-disable-next-line */}
-          <th>Purchase Price</th>
-          <th>Qty</th>
-          <th>CMP</th>
-        </tr>
-      </thead>
-      <tbody>
-        {holdings.map((stock, i) => (
-          <tr key={i}>
-            <td>{stock.particulars}</td>
-            <td>{stock.purchasePrice}</td>
-            <td>{stock.qty}</td>
-            <td>{stock.cmp}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}
+export const portfolioData = holdings;
